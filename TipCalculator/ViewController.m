39cffea955de22be7236f16032576641af863db0
Swati Wadhera <Swati@swatis-macbook-pro.local>
2016-09-24 13:01:50 +0530

    Keyboard Up Changes

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -71,7 +71,7 @@
     [doneBtn setTitle:@"Done" forState:UIControlStateNormal];
     [doneBtn addTarget:self.view action:@selector(endEditing:) forControlEvents:UIControlEventTouchUpInside];
     [doneBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [doneBtn setFrame:CGRectMake(0, self.view.bounds.size.height - 165 - self.navigationController.navigationBar.bounds.size.height - 10 - 40, self.view.bounds.size.width, 40)];
+    [doneBtn setFrame:CGRectMake(0, self.view.bounds.size.height - 160 - self.navigationController.navigationBar.bounds.size.height - 10 - 40, self.view.bounds.size.width, 40)];
     [self.view addSubview:doneBtn];
     return YES;
 }
